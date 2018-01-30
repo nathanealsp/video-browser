fc1b6d78a3f9e985c74f5ac461246583d4f2a2fe
Nytheneals <nythes@gmail.com>
2018-01-29 23:51:44 -0500

    Added YTSEARCH API & KEY to the main (Parent Component)

--- src/index.js
+++ src/index.js
@@ -6,8 +6,20 @@ import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 import _ from 'lodash';
 
+// YOUTUBE API KEY
+const API_KEY = 'AIzaSyD4bMDbBJknFVA3XVFwTysAs8jmHZ0fvzY';
+
 // CREATE NEW COMPONENT
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { video: [] };
+    // YOUTUBE API SEARCH
+    YTSearch({ key: API_KEY, term: 'chris brown' }, (data) => {
+      this.setState({ video: data });
+    });
+  }
   render() {
     return (
       <div>
