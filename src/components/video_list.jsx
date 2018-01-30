5fe2b71eaaba76cb3823a4c43c6c28fcfda49965
Nytheneals <nythes@gmail.com>
2018-01-30 11:23:26 -0500

    Edited Video_detail, and Video_list/Video_list_item

--- src/components/video_list.jsx
+++ src/components/video_list.jsx
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import VideoListItem from './video_list_item';
+import React, { Component } from "react";
+import VideoListItem from "./video_list_item";
 
-// CREATE NEW COMPONENT
+// CREATE NEW COMPONENT VIDEO LIST, ADDED IDENTIFIER KEY.
 class VideoList extends Component {
   render() {
     const { videos } = this.props; // DESTRUCTURING IN ES6
-    const mappedVideos = videos.map(video => <VideoListItem key={video.etag} video={video} />); // CREATING A LIST OF COMPONENTS
+    const mappedVideos = videos.map(video => (
+      <VideoListItem key={video.etag} video={video} />
+    )); // CREATING A LIST OF COMPONENTS
     return (
       <div>
         <ul className="col-md-4 list-group">{mappedVideos} </ul>
-        {/* <VideoListItem /> */}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail';
-import _ from 'lodash';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
+import _ from "lodash";
 
 // YOUTUBE API KEY
-const API_KEY = 'AIzaSyD4bMDbBJknFVA3XVFwTysAs8jmHZ0fvzY';
+const API_KEY = "AIzaSyD4bMDbBJknFVA3XVFwTysAs8jmHZ0fvzY";
 
 // CREATE NEW COMPONENT
 class App extends Component {
@@ -18,7 +18,7 @@ class App extends Component {
     this.state = { videos: [] };
 
     // YOUTUBE API SEARCH
-    YTSearch({ key: API_KEY, term: 'chris brown' }, (data) => {
+    YTSearch({ key: API_KEY, term: "info graphics show " }, data => {
       this.setState({ videos: data });
     });
   }
@@ -26,11 +26,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        {/* <VideoDetail /> */}
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
   }
 }
 // PUT COMPONENT INTO THE DOM
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector(".container"));
